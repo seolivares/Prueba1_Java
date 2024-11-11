8aa2936e9457f9efb3090f920e37e5f3ae35a07a
Simon <seolivares@gmail.com>
2024-11-11 02:26:51 -0300

    Se agrega el programa de la prueba 1 de Clinica Contigo Aprendo

---
+++ src/entidad/Paciente.java
@@ -0,0 +1,148 @@
+package entidad;
+
+public class Paciente {
+    //ATRIBUTOS
+    private String dni;
+    private String nombre;
+    private int edad;
+    private char sexo;
+    private double altura;
+    private int[] pesoMensual;
+
+
+    public Paciente(String dni, String nombre, int edad, char sexo, double altura) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.altura = altura;
+        this.pesoMensual = new int[12];
+
+        for (int i=0;i<pesoMensual.length;i++){
+            pesoMensual[i] = (int) (Math.random()*(250-70+1)+70) ;//random entre 70 a 250
+        }
+    }
+
+    public Paciente() {
+
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public int[] getPesoMensual() {
+        return pesoMensual;
+    }
+
+    public void setPesoMensual(int[] pesoMensual) {
+        this.pesoMensual = pesoMensual;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public int menorPesoMensual() {
+        int contMes = 0;
+
+        int pesoMenor = 250;
+
+        for(int i=0;i<pesoMensual.length;i++) {
+            if(pesoMensual[i] < pesoMenor){
+                pesoMenor = pesoMensual[i];
+                contMes = i+1;
+            }
+        }
+
+        return contMes;
+    }
+
+    public double indiceMasaCorporal(int mesX) {
+        double indiceMes = 0;
+
+        indiceMes = pesoMensual[mesX-1] / (altura*altura);
+
+        return indiceMes;
+    }
+
+    public int[] menosDeXKilos(int Xkilos){
+        //int[] pesaMenos;
+
+        int contador = 0;
+
+        // Contar cuántos meses para nuevo arreglo
+        for (int i = 0; i < pesoMensual.length; i++) {
+            if (pesoMensual[i] < Xkilos) {
+                contador++;
+            }
+        }
+
+        if (contador == 0) {
+            return null;
+        }else{
+            int[] mesPesaMenos = new int[contador];
+            int indice = 0;
+
+            for (int i = 0; i < pesoMensual.length; i++) {
+                if (pesoMensual[i] < Xkilos) {
+                    mesPesaMenos[indice] = i + 1;
+                    indice++;
+                }
+            }
+
+            return mesPesaMenos;
+        }
+    }
+
+    public String pesoMesX(double indiceMC) {
+        String condicion = "";
+
+        if(indiceMC >= 15 && indiceMC < 20){
+            condicion = "Normal";
+        }
+
+        if(indiceMC >= 20 && indiceMC < 28){
+            condicion = "Sobrepeso";
+        }
+
+        if(indiceMC >= 28){
+            condicion = "Obeso";
+        }
+
+        return condicion;
+    }
+
+}
---
+++ src/vista/App.java
@@ -0,0 +1,128 @@
+package vista;
+
+import entidad.Paciente;
+
+public class App {
+
+    public static void main(String[] args) {
+        Paciente paciente = null;
+
+        int opcion;
+
+        do {
+            opcion = menu();
+
+            switch (opcion) {
+                case 1:
+                    //Se ingresan datos del paciente
+                    paciente = crearPaciente();
+                    break;
+                case 2:
+                    //Se muestran los datos
+                    if(paciente == null){
+                        System.out.println("No hay datos, por favor ingresarlos");
+                        System.out.println();
+                    }else {
+                        mostrarInfoPaciente(paciente);
+                    }
+                    break;
+                case 3:
+                    //Calcular mes de menor peso
+                    if(paciente == null){
+                        System.out.println("No hay datos, por favor ingresarlos");
+                    }else {
+                        System.out.println("El mes de menor peso es: " + paciente.menorPesoMensual());
+                    }
+
+                    System.out.println();
+                    break;
+                case 4:
+                    //Comparacion de X peso
+                    if(paciente == null){
+                        System.out.println("No hay datos, por favor ingresarlos");
+                        System.out.println();
+                    }else {
+                        System.out.print("Ingresar Peso: ");
+                        int xKilos = Leer.datoInt();
+
+
+
+                        if(paciente.menosDeXKilos(xKilos) == null) {
+                            System.out.println("No se encontraron pesos menores a " + xKilos);
+                        }else {
+                            int[] mesesMenorPeso = paciente.menosDeXKilos(xKilos);
+
+                            System.out.println("Estos son los meses de menor peso a " + xKilos + " :");
+
+                            for(int i=0; i<mesesMenorPeso.length;i++) {
+                                System.out.println("Mes: " + mesesMenorPeso[i]);
+                            }
+                        }
+                    }
+
+                    System.out.println();
+                    break;
+                case 5:
+                    //Calcular condicion
+                    if(paciente == null){
+                        System.out.println("No hay datos, por favor ingresarlos");
+                        System.out.println();
+                    }else {
+                        System.out.print("Ingresar Mes: ");
+                        int mesX = Leer.datoInt();
+                        double indice = paciente.indiceMasaCorporal(mesX);
+                        System.out.println("El indice de masa corporal es: " + indice);
+                        System.out.println("La condición del paciente es: " + paciente.pesoMesX(indice));
+                    }
+
+                    System.out.println();
+                    break;
+            }
+
+        } while(opcion!=6);
+    }
+
+    public static int menu(){
+        System.out.println("*** Sistema de Control de Peso ***");
+        System.out.println("==================================");
+        System.out.println("1.\tIngresar datos del paciente");
+        System.out.println("2.\tMostrar datos del paciente");
+        System.out.println("3.\tMostrar el mes con menor peso");
+        System.out.println("4.\tIngresar peso de comparación por mes");
+        System.out.println("5.\tMostrar condición de un mes");
+        System.out.println("6.\tSalir");
+        System.out.println("==================================");
+        System.out.println("Favor ingrese una opción para continuar: ");
+        return Leer.datoInt();
+    }
+
+    public static Paciente crearPaciente(){
+        System.out.println("Por favor ingrese el dni");
+        String dni = Leer.dato();
+        System.out.println("Por favor ingrese el nombre");
+        String nombre = Leer.dato();
+        System.out.println("Por favor ingrese la estatuta");
+        double altura = Leer.datoDouble();
+        System.out.println("Por favor ingrese la edad");
+        int edad = Leer.datoInt();
+        System.out.println("Por favor ingrese el sexo");
+        char sexo = Leer.datoChar();
+
+        return new Paciente(dni,nombre,edad,sexo,altura);
+    }
+
+    public static void mostrarInfoPaciente(Paciente paciente) {
+        System.out.println("Datos de Paciente:");
+        System.out.println("DNI: " + paciente.getDni());
+        System.out.println("Nombre: " + paciente.getNombre());
+        System.out.println("Edad: " + paciente.getEdad());
+        System.out.println("Sexo: " + paciente.getSexo());
+        System.out.println("Altura: " + paciente.getAltura());
+
+        for(int i=0; i<paciente.getPesoMensual().length;i++) {
+            System.out.println("El Peso del mes: " + (i+1) + " es: " + paciente.getPesoMensual()[i] + " Kg");
+        }
+
+        System.out.println();
+    }
+}
